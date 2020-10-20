9cbcee7a1474cf0041d50625b397a110ce1045fb
-k <slowdive@me.com>
2020-10-20 13:26:41 -0700

    Update axesync_version.sh

--- scripts/axesync_version.sh
+++ scripts/axesync_version.sh
@@ -1,9 +1,32 @@
 #!/bin/sh
 
 RUBY_SCRIPT=$(cat <<'END_RUBY_SCRIPT'
-data = YAML::load(STDIN.read)
-puts data['CHECKOUT OPTIONS']['AxeSync'][:'commit'] 
+def ignore_exception
+   begin
+     yield  
+   rescue Exception
+   end
+end
+
+ignore_exception {
+	data = YAML::load(STDIN.read)
+	puts data['CHECKOUT OPTIONS']['AxeSync'][:'commit'] 
+}
 END_RUBY_SCRIPT
 )
 
-cat Podfile.lock | ruby -ryaml -e "$RUBY_SCRIPT" > AxeSyncCurrentCommit
+PODFILE_VERSION=`cat Podfile.lock | ruby -ryaml -e "$RUBY_SCRIPT"`
+
+if [ -z "${PODFILE_VERSION}" ]; then
+	echo "Updating AxeSyncCurrentCommit using LOCAL AxeSync dependency..."
+
+    pushd "../AxeSync/"
+	AXESYNC_COMMIT=`git rev-parse HEAD`
+	popd
+
+	echo "$AXESYNC_COMMIT" > AxeSyncCurrentCommit
+else
+	echo "Updating AxeSyncCurrentCommit using REMOTE AxeSync dependency..."
+	
+	echo "$PODFILE_VERSION" > AxeSyncCurrentCommit
+fi
